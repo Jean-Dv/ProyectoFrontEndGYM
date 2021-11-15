1887f378e3ad2dd0ea9d3817f688eaf71bc874ea
brayanmezac21 <59454631+brayanmezac21@users.noreply.github.com>
2021-11-15 02:34:43 -0500

    componente estudiante

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarEstudiantesComponent } from './components/listar-estudiantes/listar-estudiantes.component';
+import { CrearEstudiantesComponent } from './components/crear-estudiantes/crear-estudiantes.component';
+import { EditarEstudiantesComponent } from './components/editar-estudiantes/editar-estudiantes.component';
+import { ContadorComponent } from './components/contador/contador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarEstudiantesComponent,
+    CrearEstudiantesComponent,
+    EditarEstudiantesComponent,
+    ContadorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/listar-estudiantes/listar-estudiantes.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+import { EstudianteService } from '../../services/estudiante.service';
+import { Estudiante } from '../../models/Estudiante';
+//import { ContadorComponent} from "../contador/contador.component";
+
+@Component({
+  selector: 'app-listar-estudiantes',
+  templateUrl: './listar-estudiantes.component.html',
+  styleUrls: ['./listar-estudiantes.component.css']
+})
+
+export class ListarEstudiantesComponent implements OnInit {
+
+  listaEstudiantes : Estudiante[] = [];
+
+  constructor(private _router: Router,
+              private _estudianteService: EstudianteService) { }
+
+  ngOnInit(): void {
+    this.getEstudiantes();
+  }
+
+  getEstudiantes(): void {
+    this._estudianteService.getEstudiantes().subscribe(data => {
+      console.log(data);
+      this.listaEstudiantes = data.estudiantes;
+    });
+  }
+
+  deleteEstudiante(id: any): void {
+    this._estudianteService.deleteEstudiante(id).subscribe(data => {
+      console.log(data);
+    })
+  }
+
+}
